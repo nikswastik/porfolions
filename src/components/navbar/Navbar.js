e813df56c07e68d2e885107d41da3567d775b07f
SPURGE <apple@Apples-MacBook-Pro.local>
2022-11-09 18:57:42 +0530

    responsivess

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -8,11 +8,10 @@ export default function Navbar() {
 
   return (
     <div className='navbar-cntr'>
-      <div class="separator">
-        <div class="line"></div>
+      
         <h2 className='lang-text'>Swastik Naik</h2>
-        <div class="line"></div>
-      </div>
+     
+      
     </div>
   )
 }
